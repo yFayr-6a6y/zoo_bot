2f8c8e8df0ad7ed6bc5c65518121b5003545df26
yFayr-6a6y <kakanop7xiaomi@gmail.com>
2026-03-19 13:51:32 +0300

    new

---
+++ src/test/java/com/example/zoo_bot/ZooBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zoo_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
